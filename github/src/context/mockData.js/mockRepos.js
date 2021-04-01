15c1975994216b72565852d117513914ac088e93
Mtaaha94 <muhammadtaaha.94@gmail.com>
2021-04-01 21:53:08 +0500

    first commit

--- github/src/context/mockData.js/mockRepos.js
+++ github/src/context/mockData.js/mockRepos.js
@@ -1,113 +1,113 @@
 export default [
   {
-    id: 239164394,
-    node_id: "MDEwOlJlcG9zaXRvcnkyMzkxNjQzOTQ=",
-    name: "bazinga-example",
-    full_name: "john-smilga/bazinga-example",
+    id:42133387 ,
+    node_id: "",
+    name: "",
+    full_name: "",
     private: false,
     owner: {
-      login: "john-smilga",
+      login: '',
       id: 42133389,
-      node_id: "MDQ6VXNlcjQyMTMzMzg5",
-      avatar_url: "https://avatars3.githubusercontent.com/u/42133389?v=4",
+      node_id: "",
+      avatar_url: "",
       gravatar_id: "",
-      url: "https://api.github.com/users/john-smilga",
-      html_url: "https://github.com/john-smilga",
-      followers_url: "https://api.github.com/users/john-smilga/followers",
+      url: "",
+      html_url: "",
+      followers_url: "",
       following_url:
-        "https://api.github.com/users/john-smilga/following{/other_user}",
-      gists_url: "https://api.github.com/users/john-smilga/gists{/gist_id}",
+        "",
+      gists_url: "",
       starred_url:
-        "https://api.github.com/users/john-smilga/starred{/owner}{/repo}",
+        "",
       subscriptions_url:
-        "https://api.github.com/users/john-smilga/subscriptions",
-      organizations_url: "https://api.github.com/users/john-smilga/orgs",
-      repos_url: "https://api.github.com/users/john-smilga/repos",
-      events_url: "https://api.github.com/users/john-smilga/events{/privacy}",
+        "",
+      organizations_url: "",
+      repos_url: "",
+      events_url: "",
       received_events_url:
-        "https://api.github.com/users/john-smilga/received_events",
-      type: "User",
+        "",
+      type: "",
       site_admin: false,
     },
-    html_url: "https://github.com/john-smilga/bazinga-example",
+    html_url: "",
     description: null,
     fork: false,
-    url: "https://api.github.com/repos/john-smilga/bazinga-example",
-    forks_url: "https://api.github.com/repos/john-smilga/bazinga-example/forks",
+    url: "",
+    forks_url: "",
     keys_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/keys{/key_id}",
+      "",
     collaborators_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/collaborators{/collaborator}",
-    teams_url: "https://api.github.com/repos/john-smilga/bazinga-example/teams",
-    hooks_url: "https://api.github.com/repos/john-smilga/bazinga-example/hooks",
+      "",
+    teams_url: "",
+    hooks_url: "",
     issue_events_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/issues/events{/number}",
+      "",
     events_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/events",
+      "",
     assignees_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/assignees{/user}",
+      "",
     branches_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/branches{/branch}",
-    tags_url: "https://api.github.com/repos/john-smilga/bazinga-example/tags",
+      "",
+    tags_url: "",
     blobs_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/git/blobs{/sha}",
+      "",
     git_tags_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/git/tags{/sha}",
+      "",
     git_refs_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/git/refs{/sha}",
+      "",
     trees_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/git/trees{/sha}",
+      "",
     statuses_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/statuses/{sha}",
+      "",
     languages_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/languages",
+      "",
     stargazers_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/stargazers",
+      "",
     contributors_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/contributors",
+      "",
     subscribers_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/subscribers",
+      "",
     subscription_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/subscription",
+      "",
     commits_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/commits{/sha}",
+      "",
     git_commits_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/git/commits{/sha}",
+      "",
     comments_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/comments{/number}",
+      "",
     issue_comment_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/issues/comments{/number}",
+      "",
     contents_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/contents/{+path}",
+      "",
     compare_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/compare/{base}...{head}",
+      "",
     merges_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/merges",
+      "",
     archive_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/{archive_format}{/ref}",
+      "",
     downloads_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/downloads",
+      "",
     issues_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/issues{/number}",
+      "",
     pulls_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/pulls{/number}",
+      "",
     milestones_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/milestones{/number}",
+      "",
     notifications_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/notifications{?since,all,participating}",
+      "",
     labels_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/labels{/name}",
+      "",
     releases_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/releases{/id}",
+      "",
     deployments_url:
-      "https://api.github.com/repos/john-smilga/bazinga-example/deployments",
-    created_at: "2020-02-08T16:30:10Z",
-    updated_at: "2020-05-12T15:57:33Z",
-    pushed_at: "2020-02-08T16:30:48Z",
-    git_url: "git://github.com/john-smilga/bazinga-example.git",
-    ssh_url: "git@github.com:john-smilga/bazinga-example.git",
-    clone_url: "https://github.com/john-smilga/bazinga-example.git",
-    svn_url: "https://github.com/john-smilga/bazinga-example",
+      "",
+    created_at: "",
+    updated_at: "",
+    pushed_at: "",
+    git_url: "",
+    ssh_url: "",
+    clone_url: "",
+    svn_url: "",
     homepage: null,
     size: 173,
     stargazers_count: 1,
